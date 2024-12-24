c732741e1c0de33c0e6ecd2f16c78dcc4fbd148d
Lelamakh <makhokhashvililela@gmail.com>
2024-12-24 13:08:15 -0500

    added data and show the specific info in the HTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,166 +1,51 @@
-// const createUserUrl = "https://borjomi.loremipsum.ge/api/register", //method POST  ყველა ველი სავალდებულო
-//   getAllUsersUrl = "https://borjomi.loremipsum.ge/api/all-users", //method GET
-//   getSingleUserUrl = "https://borjomi.loremipsum.ge/api/get-user/120 ", //id, method  GET
-//   updateUserUrl = "https://borjomi.loremipsum.ge/api/update-user/145 ", //id, method PUT  ყველა ველი სავალდებულო
-//   deleteUserUrl = "https://borjomi.loremipsum.ge/api/delete-user/"; //id, method DELETE
-// const
-// const regForm = document.querySelector("#reg"),
-//   userName = document.querySelector("#user_name"),
-//   userSurname = document.querySelector("#user_surname"),
-//   userEmail = document.querySelector("#user_email"),
-//   userPhone = document.querySelector("#user_phone"),
-//   userPersonalID = document.querySelector("#user_personal-id"),
-//   userZip = document.querySelector("#user_zip-code"),
-//   userGender = document.querySelector("#user_gender"),
-//   // user id ფორმში, რომელიც გვჭირდება დაედითებისთვის
-//   user_id = document.querySelector("#user_id");
+const body = document.querySelector("body");
+const section = document.createElement("section");
 
-// const user = {
-//   first_name: "satesto",
-//   last_name: "text",
-//   phone: "123456789",
-//   id_number: "12345678909",
-//   email: "text@gmail.com",
-//   gender: "male",
-//   zip_code: "1245",
-// };
+const newSection = document.createElement("section");
+body.append(newSection);
 
-// new Promise((resolve, reject) => {
-//   if (false) {
-//     resolve("success");
-//   } else {
-//     reject("error");
-//   }
-// })
-//   .then((data) => {
-//     console.log(data);
-//   })
-//   .catch((data) => {
-//     console.log(data);
-//   });
-
-// // async await
-
-// // fetch().then().then().catch()
-
-// // try {
-// // 	console.log("try");
-// // 	regForm.addEventListener("submit", (e) => {});
-// // } catch (error) {
-// // 	console.log(error);
-// // } finally {
-// // 	console.log("finally");
-// // }
-
-// console.log("after try catch");
-// function getAllUsers() {
-//   // add loading spinner
-//   fetch("https://borjomi.loremipsum.ge/api/all-users")
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data.users);
-//       // render users
-//       const html = data.users
-//         .map((el) => {
-//           return `
-//          <div class="user"></div>
-//         `;
-//         })
-//         .join("");
-
-//       // section.innerHtml = html
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     })
-//     .finally(() => {
-//       // remove loading spinner
-//       console.log("finally");
-//     });
-// }
-
-// function createUser(user) {
-//   fetch("https://borjomi.loremipsum.ge/api/register", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(user),
-//   })
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     })
-//     .finally(() => {
-//       console.log("finally");
-//     });
-// }
-
-// function deleteUser(userId) {
-//   fetch(`${deleteUserUrl}${userId}`, {
-//     method: "DELETE",
-//   })
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     })
-//     .finally(() => {
-//       console.log("finally");
-//     });
-// }
-
-// function getUser(userId) {
-//   fetch(`https://borjomi.loremipsum.ge/api/get-user/${userId}`)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     })
-//     .finally(() => {
-//       console.log("finally");
-//     });
-// }
-
-// getAllUsers();
-
-// regForm.addEventListener("submit", (e) => {
-//   e.preventDefault();
-
-//   // validations
-
-//   const user = {
-//     first_name: userName.value,
-//     last_name: userSurname.value,
-//     phone: userPhone.value,
-//     id_number: userPersonalID.value,
-//     email: userEmail.value,
-//     gender: userGender.value,
-//     zip_code: userZip.value,
-//   };
-
-//   if (true) {
-//     console.log(user);
-//     createUser(user);
-
-//     // getAllUsers();
-//   }
-// });
-
-fetch(" https://jsonplaceholder.typicode.com/users")
-  .then((response) => {
-    console.log(response);
-    return response.json();
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((Response) => {
+    console.log(Response);
+    return Response.json();
   })
   .then((data) => {
     console.log(data);
+
+    const dataToUse = data;
+    console.log(dataToUse);
+    const logData = (el, index, arr) => {
+      console.log(el, index, arr);
+    };
+
+    const formatedArray = dataToUse.map((el, index, arr) => {
+      return {
+        name: el.name,
+        email: el.email,
+        address: el.address.street,
+      };
+    });
+
+    function renderHTML(data) {
+      const formatedData = data.map(
+        (el) =>
+          `<div class="userInfo">
+            <h2>${el.name}</h2>
+            <p>${el.email}</p>
+            <p>${
+              el.address.street +
+              ", " +
+              el.address.suite +
+              ", " +
+              el.address.city
+            }</p>
+               
+          </div>`
+      );
+      return formatedData.join(" ");
+    }
+
+    newSection.innerHTML = renderHTML(data);
   })
   .catch((error) => {
     console.log(error);
